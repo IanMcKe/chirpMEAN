2443661b97ec9777f82efe6a18e687198654a43d
Ian McKenney <ian.airforde@gmail.com>
2015-10-28 11:52:43 -0700

    fix file structure and remove uneccessary html files

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -3,21 +3,21 @@ var chirpApp = angular.module('chirpApp', ['ui.router', 'ngResource']);
 chirpApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "../main.html",
+    templateUrl: "../partials/main.html",
     controller: "MainController",
     controllerAs: "vm"
   });
 
   $stateProvider.state('login', {
     url: "/login",
-    templateUrl: "../login.html",
+    templateUrl: "../partials/login.html",
     controller: "AuthController",
     controllerAs: "vm"
   });
 
   $stateProvider.state('register', {
     url: "/register",
-    templateUrl: "../register.html",
+    templateUrl: "../partials/register.html",
     controller: "AuthController",
     controllerAs: "vm"
   });
